7501543050c129a808e612ed2a89e83e2baf3e0e
Charlie Kong <charlie@undercurrent.tech>
2023-04-27 16:14:11 +0800

    update variables

--- pyteal_program.py
+++ pyteal_program.py
@@ -1,7 +1,7 @@
 from pyteal import *
 
 ASSET_CREATOR = Addr("FA3CFQSFJIBPLSOM37SCQR5666DALRAORRSI5LAFDA2SICSZZ3MU6A7ALE")
-ASSET_SENDER = Addr("LHRYGXNIMZKE24FXS3A5DCTPHCA63H6HYIGITBKEQJB4JPZ3FIQ6SYUNS4")
+ASSET_RECEIVER = Addr("LHRYGXNIMZKE24FXS3A5DCTPHCA63H6HYIGITBKEQJB4JPZ3FIQ6SYUNS4")
 
 
 def pyteal_program():
@@ -10,18 +10,18 @@ def pyteal_program():
         # Asset opt in
         Assert(Gtxn[0].type_enum() == TxnType.AssetTransfer),
         Assert(Gtxn[0].asset_amount() == Int(0)),
-        Assert(Gtxn[0].sender() == ASSET_SENDER),
-        Assert(Gtxn[0].asset_receiver() == ASSET_SENDER),
+        Assert(Gtxn[0].sender() == ASSET_RECEIVER),
+        Assert(Gtxn[0].asset_receiver() == ASSET_RECEIVER),
         # Payment of 5 Algos
         Assert(Gtxn[1].type_enum() == TxnType.Payment),
         Assert(Gtxn[1].amount() == Int(5_000_000)),
-        Assert(Gtxn[1].sender() == ASSET_SENDER),
+        Assert(Gtxn[1].sender() == ASSET_RECEIVER),
         Assert(Gtxn[1].receiver() == ASSET_CREATOR),
         # Asset transfer
         Assert(Gtxn[2].type_enum() == TxnType.AssetTransfer),
         Assert(Gtxn[2].asset_amount() == Int(1)),
         Assert(Gtxn[2].sender() == ASSET_CREATOR),
-        Assert(Gtxn[2].asset_receiver() == ASSET_SENDER),
+        Assert(Gtxn[2].asset_receiver() == ASSET_RECEIVER),
         # Approve if all assertions pass
         Approve(),
     )
